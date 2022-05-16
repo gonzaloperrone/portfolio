f5deb78d59ca2d2ad9bf404cbfcd739bd70540f1
gonzaloperrone <gonzalodamianperrone@gmail.com>
2022-05-16 12:09:25 -0300

    certtifications

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ fetch("js/projects.json")
     .then(data => {
         data.forEach(project => {
             document.getElementById('portfolioCards').innerHTML += `
-            <div class="card">
-            <img src=${project.img} class="card-img-top" alt="...">
+            <div class="card projectCard">
+            <img src=${project.img} class="card-img-top" alt="${project.name}">
             <div class="card-body">
               <h4 class="card-title">${project.name}</h4>
               <small class="text-muted makeWith">${project.madeWith}</small>
@@ -12,6 +12,23 @@ fetch("js/projects.json")
               <a href="${project.demo}" class="btn btn-primary" target="_blank"><i class="bi bi-globe"></i> Demo</a>
               <a href="${project.code}" class="btn btn-primary" target="_blank"><i class="bi bi-code-slash"></i> Código</a>
             </div>
+          </div>`
+        });
+    })
+    .catch(error => console.log(error))
+
+    fetch("js/certifications.json")
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(certification => {
+            document.getElementById('certificationsCard').innerHTML += `
+            <div class="card certificationCard">
+            <img src=${certification.img} class="card-img-top" alt="${certification.name}">
+            <div class="card-body">
+              <h4 class="card-title">${certification.name}</h4>
+              <h6>${certification.platform}</h6>
+              <small class="text-muted">(${certification.year})</small>
+             </div>
           </div>`
         });
     })
